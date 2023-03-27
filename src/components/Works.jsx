e25692bed949a544019f0a9da612c7222336eda3
JSjags <jesseabuaja@hotmail.com>
2023-03-27 07:44:31 +0100

    Fixed tilt card default height.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({
         glareBorderRadius="20px"
         glareColor="violet"
         glareMaxOpacity={0.2}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[340px] w-full green-pink-gradient-shadow"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[340px] h-[100%] w-full green-pink-gradient-shadow"
       >
         <div className="relative w-full h-[230px]">
           <img
